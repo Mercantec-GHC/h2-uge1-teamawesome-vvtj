7705efed0229b6d3312c0e780278d6ed0831b42f
wivelsted <vika.forever.21@gmail.com>
2025-08-08 09:09:39 +0200

    Organized code

---
+++ BlazorWASM/Pages/Countries.razor.cs
@@ -0,0 +1,34 @@
+﻿using BlazorBootstrap;
+using BlazorWASM.Components;
+using BlazorWASM.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorWASM.Pages;
+public partial class Countries
+{
+	[Inject]
+	NavigationManager NavigationManager { get; set; } = default!;
+	[Inject]
+	APIService APIService { get; set; } = default!;
+	public List<Country>? countries;
+
+	protected override async Task OnInitializedAsync()
+	{
+		countries = await APIService.GetAllCountriesAsync() ?? new List<Country>();
+	}
+
+	private async Task<GridDataProviderResult<Country>> CountriesDataProvider(GridDataProviderRequest<Country> request)
+	{
+		return await Task.FromResult(request.ApplyTo(countries ?? Enumerable.Empty<Country>()));
+	}
+
+	public void ToggleViewButton(Country context)
+	{
+		if (context != null)
+		{
+			var country = (context as Country);
+			var navUrl = $"/countries/{country.Id}";
+			NavigationManager.NavigateTo(navUrl);
+		}
+	}
+}
---
+++ BlazorWASM/Pages/CountryDashboard.razor.cs
@@ -0,0 +1,40 @@
+﻿using BlazorWASM.Components;
+using BlazorWASM.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorWASM.Pages;
+public partial class CountryDashboard
+{
+	[Parameter]
+	public string? CountryId { get; set; }
+	[Parameter]
+	public bool IsVisible { get; set; } = false;
+	[Inject]
+	NavigationManager NavigationManager { get; set; } = default!;
+	[Inject]
+	APIService APIService { get; set; } = default!;
+	public CountryDataById? countryDashboard;
+
+	protected override async Task OnInitializedAsync()
+	{
+		if (string.IsNullOrEmpty(CountryId))
+		{
+			NavigateToList();
+			return;
+		}
+		countryDashboard = await APIService.GetCountryDataByIdAsync(CountryId) ?? new CountryDataById();
+		IsVisible = true;
+	}
+	public void NavigateToList()
+	{
+		var navUrl = $"/countries";
+		NavigationManager.NavigateTo(navUrl);
+
+	}
+	public void NavigateToMain()
+	{
+		var navUrl = $"/";
+		NavigationManager.NavigateTo(navUrl);
+
+	}
+}
